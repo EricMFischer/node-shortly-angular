79bf3c754f6a205132269814ef9c9333448d6a54
Hack Reactor Students <accounts@hackreactor.com>
2015-07-31 19:57:57 -0700

    Implements form validations

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,11 +2,13 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
-  
+  $scope.urlRegex = "!\"#$%&\'()*+,-./@:;<=>[\\]^_`{|}~";
   $scope.addLink = function() {
     Links.post($scope.link).then(function(link){
       console.log(link);
+      $scope.link = {};
     });
   }
+
   // $scope.addLink();
 });
