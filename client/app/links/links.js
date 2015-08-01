58181e3175ab06da1ef995d6393292471e12f2fd
Hack Reactor Students <accounts@hackreactor.com>
2015-08-01 11:38:43 -0700

    Live search works

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,6 +2,7 @@ var app = angular.module('shortly.links', []);
 
 app.controller('LinksController', function ($scope, Links) {
   $scope.data = {};
+  $scope.text = '';
 
   $scope.getLinks = function(){
     Links.get().then(function(data){
@@ -9,4 +10,20 @@ app.controller('LinksController', function ($scope, Links) {
     });
   }
   $scope.getLinks();
+});
+
+app.filter('searchFor', function() {
+
+  return function(array, text) {
+    var result = [];
+    if (!text) {return array;}
+    else {
+      for (var i=0; i<array.length; i++) {
+        if (array[i].title.toLowerCase().indexOf(text) !== -1) {
+          result.push(array[i]);
+        }
+      }
+    }
+    return result;
+  };
 });
